9c1fb80b9e44ca039ec904f09785d3fb1ecd1e1f
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2023-09-26 20:01:13 -0300

    Added (sort of) easter egg

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+let lastname = document.getElementById("lastname");
 
+lastname.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    e.target.parentNode.style.fontFamily = "Fenix, serif";
+});
