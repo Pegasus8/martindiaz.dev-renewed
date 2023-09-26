1ca4e2905a5f4f680382cbe7ad0722de7677d6ef
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2023-09-26 19:59:48 -0300

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        // Use the following in case of React
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
