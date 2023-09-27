0bc8ab37d09b316e3f3f809944339cdc4b655ac6
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2023-09-26 21:24:03 -0300

    Removed "easter egg"

--- main.js
+++
@@ -1,7 +0,0 @@
-let lastname = document.getElementById("lastname");
-
-lastname.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    e.target.parentNode.style.fontFamily = "Fenix, serif";
-});
